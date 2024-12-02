2102f4145f4344a86c224101fa709b10fa283263
Gambozza <gabriele.rola@studenti.unimi.it>
2024-12-02 13:00:18 +0100

    nulla di che, leggi il messaggio che ho lasciato quando ho fatto il commit del primo file

--- listaConcatenata.c
+++ listaConcatenata.c
@@ -9,45 +9,48 @@ membri delle strutture ricevute in input (senza implementazione)
 
 ***************************************************************************/
 
-typedef struct key
+#define STRLEN 16
+struct key
 {
-    char codice[50]; // Chiave di ricerca
+    char codice[STRLEN]; // Chiave di ricerca
 } key;
+typedef struct key key;
 
-typedef struct item
+struct item
 {
     key chiave;           // Chiave per la ricerca
     int valint;           // Valore intero
     unsigned long int ID; // Codice ID
 } item;
+typedef struct item item;
 
-typedef struct nodo
+struct nodo
 {
     item dati;         // Dati satellite
     struct nodo *next; // Puntatore al prossimo nodo
 } nodo;
+typedef struct itemlist itemlist;
 
-nodo *list_search(nodo *head, key k);
-
+item list_search(itemlist * head, key k);
 /************************************************************************************************************************************************************************************
 
-La procedura di ricerca in una lista concatenata è un processo iterativo che esplora 
-sequenzialmente ogni nodo della lista a partire dalla sua testa, cioè il primo elemento. 
-La funzione di ricerca riceve in input un puntatore al primo nodo della lista 
-e una chiave, che rappresenta il valore da trovare. 
+La procedura di ricerca in una lista concatenata è un processo iterativo che esplora
+sequenzialmente ogni nodo della lista a partire dalla sua testa, cioè il primo elemento.
+La funzione di ricerca riceve in input un puntatore al primo nodo della lista (head)
+e una chiave (k), che rappresenta il valore da trovare.
 L’obiettivo è individuare il nodo il cui campo chiave corrisponda esattamente al valore fornito.
 
-La logica consiste nell'utilizzare un ciclo per attraversare la lista, partendo dal nodo iniziale e 
-procedendo attraverso i collegamenti definiti dal puntatore al nodo successivo. 
-Per ciascun nodo visitato, si estrae il valore della chiave associata e lo si confronta con la chiave di ricerca. 
-Questo confronto viene effettuato utilizzando una funzione specifica, progettata per determinare se due chiavi siano uguali. 
-Se il confronto restituisce un risultato positivo, significa che il nodo corrente contiene i dati cercati, 
+La logica consiste nell'utilizzare un ciclo per attraversare la lista, partendo dal nodo iniziale e
+procedendo attraverso i collegamenti definiti dal puntatore al nodo successivo.
+Per ciascun nodo visitato, si estrae il valore della chiave associata e lo si confronta con la chiave di ricerca.
+Questo confronto viene effettuato utilizzando una funzione specifica, progettata per determinare se due chiavi siano uguali.
+Se il confronto restituisce un risultato positivo, significa che il nodo corrente contiene i dati cercati,
 e il puntatore al nodo viene restituito come risultato della funzione.
 
-Nel caso in cui il nodo corrente non corrisponda alla chiave di ricerca, 
-il processo prosegue passando al nodo successivo. 
-Questo ciclo continua fino a quando non si trova un nodo corrispondente oppure si raggiunge la fine della lista, 
-ovvero un nodo il cui puntatore successivo è nullo. Se la lista viene completamente attraversata senza trovare una corrispondenza, 
+Nel caso in cui il nodo corrente non corrisponda alla chiave di ricerca,
+il processo prosegue passando al nodo successivo.
+Questo ciclo continua fino a quando non si trova un nodo corrispondente oppure si raggiunge la fine della lista,
+ovvero un nodo il cui puntatore successivo è nullo. Se la lista viene completamente attraversata senza trovare una corrispondenza,
 la funzione restituisce un valore nullo per indicare che la chiave non è presente nella lista.
 
 ************************************************************************************************************************************************************************************/
